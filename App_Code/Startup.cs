af5dccf5a05184ebd8fc435b153b6fbebe1afded
Rowe134 <1347636138@qq.com>
2017-11-05 22:16:31 +0800

    提交测试

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Education_SYS.Startup))]
+namespace Education_SYS
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
